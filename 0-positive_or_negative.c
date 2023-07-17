c0012795e1dd57d8dbe0e258eec0a8054106baec
Ma-ay <136788854+Ma-ay@users.noreply.github.com>
2023-07-18 05:25:33 +0800

    Update 0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,56 +1,32 @@
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
- *
- * Return: Always 0 (Success)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
-<<<<<<< HEAD
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-if  (n > 0
-
-				else if (n == 0)
-
-						{
-
-									printf("%d is zero\n", n);
-
-										}
-
-					else
-
-							{
-
-										printf("%d is negative\n", n);
-
-											}
-
-						return (0);
-
-}
-				return (0);
-=======
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%i is zero\n", n);
 	}
-	else
+	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 	}
+
 	return (0);
 }
