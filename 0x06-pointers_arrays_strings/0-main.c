4e0c4bed9dc020a9e86782a63501da5938deec24
Ma-ay <136788854+Ma-ay@users.noreply.github.com>
2023-07-30 01:09:32 +0800

    Update 0-main.c

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -1,50 +1,4 @@
-root@c58493a57251:/# cd alx-low_level_programming
-root@c58493a57251:/alx-low_level_programming# ls
-0x00-hello_world
-0x01-variables_if_else_while
-0x02-functions_nested_loops
-0x05-pointers_arrays_strings
-0x06-pointers_arrays_strings
-README.md
-root@c58493a57251:/alx-low_level_programming# cd 0x02-functions_nested_loops
-root@c58493a57251:/alx-low_level_programming/0x02-functions_nested_loops# ls
-0-putchar.c  _putchar    README.md
-main.h       _putchar.c
-root@c58493a57251:/alx-low_level_programming/0x02-functions_nested_loops# catbmain.h
-bash: catbmain.h: command not found
-root@c58493a57251:/alx-low_level_programming/0x02-functions_nested_loops# Cat main.h
-bash: Cat: command not found
-root@c58493a57251:/alx-low_level_programming/0x02-functions_nested_loops# ls main.h
-root@c58493a57251:/alx-low_level_programming/0x02-froot@c58493a57251:/alx-low_level_programming/0x02-f
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-#ifndef MAIN_H_INCLUDED
-unctions_nested_loops# cd..
-bash: cd..: command not found
-root@c58493a57251:/alx-low_level_programming/0x02-functions_nested_loops# cd ..
-root@c58493a57251:/alx-low_level_programming# ls
-\**
-0x00-hello_world
-0x01-variables_if_else_while
-0x02-functions_nested_loops
-0x05-pointers_arrays_strings
-0x06-pointers_arrays_strings
-README.md
-root@c58493a57251:/alx-low_level_programming# CDs 0x06-pointers_arrays_strings
-bash: CDs: command not found
-root@c58493a57251:/alx-low_level_programming# cd 0x06-pointers_arrays_strings
-root@c58493a57251:/alx-low_level_programming/0x06-pointers_arrays_strings# ls
-0-strcat.c
-root@c58493a57251:/alx-low_level_programming/0x06-pointers_arrays_strings# vi main.h file
-2 files to edit
-root@c58493a57251:/alx-low_level_programming/0x06-pointers_arrays_strings# 0-strcat.c
-bash: 0-strcat.c: command not found
-root@c58493a57251:/alx-low_level_programming/0x06-pointers_arrays_strings# vi 0-strcat.c
-root@c58493a57251:/alx-low_level_programming/0x06-pointers_arrays_strings# git add .
-root@c58493a57251:/alx-low_level_programming/0x06-pointers_arrays_strings# git commit -m " 0-strcat.c"[master 6f49215]  0-strcat.c
- 2 files changed, 15 insertions(+), 1 deletion(-)
- create mode 100644 0x06-pointers_arrays_strings/main.h
-include "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -65,4 +19,4 @@ int main(void)
     printf("%s", s2);
     printf("%s", ptr);
     return (0);
-} 
+}
